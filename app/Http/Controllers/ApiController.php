57543fdd82dc43c2a080f9fcba4df4068a3a3217
James Golz <jmgolz@gmail.com>
2017-01-24 20:24:51 -0500

    update packages, other fun things

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,24 +1,41 @@
 <?php
 namespace App\Http\Controllers;
+putenv('GOOGLE_APPLICATION_CREDENTIALS='.__DIR__.'/../firebase.json');
 
 use Illuminate\Http\Request;
+use Kreait\Firebase\Configuration;
+use Kreait\Firebase\Firebase;
 
 class ApiController extends Controller
 {
     private $mock_products = array(
-        array('name' => 'product one'),
-        array('name' => 'product two')
+        "products" => array(
+            array(
+                'title' => 'product one',
+                'description' => 'this is product one',
+                'in_stock' => false
+            ),
+            array(
+                'title' => 'product two',
+                'description' => 'this is product TWO',
+                'in_stock' => true
+            )
+        )
     );
     
+    private $firebase_client;
+    private $firebase_config;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
-        //
-        
+    public function __construct(){
+        $this->firebase_config = new Configuration();
+        $this->firebase_config->setAuthConfigFile(__DIR__.'/../firebase.json');
+
+        $this->firebase_client = new Firebase('https://meow-api.firebaseio.com/', $this->firebase_config);
+
     }
 
     public function get_all_products(){
