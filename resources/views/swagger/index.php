913cd9c06cca4c30ea321e33ff72158fd47426e1
James Golz <jmgolz@gmail.com>
2017-02-01 12:17:22 -0500

    switch hosts

--- resources/views/swagger/index.php
+++ resources/views/swagger/index.php
@@ -38,8 +38,8 @@
       if (url && url.length > 1) {
         url = decodeURIComponent(url[1]);
       } else {
-        // url = "https://meow-api.herokuapp.com/swagger.json";
-        url = "http://localhost:8000/swagger.json";
+        url = "https://meow-api.herokuapp.com/swagger.json";
+        // url = "http://localhost:8000/swagger.json";
       }
 
       hljs.configure({
